8c23895c4189c6e8f7814447556113017c98400c
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-29 19:34:57 -0300

    feat: implement layout component with routing and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './template/layout/layout.component';
 
 @Component({
   selector: 'app-root',
