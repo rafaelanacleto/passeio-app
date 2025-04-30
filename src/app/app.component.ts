1d887347924448599078b7cccbc5d4a7aca9caac
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-04-30 11:33:39 -0300

    feat: add routing for categorias and implement CategoriaComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { LayoutComponent } from './template/layout/layout.component';
 
 @Component({
   selector: 'app-root',
--- src/app/template/template-routing.module.ts
+++ src/app/template/template-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'categorias',
+        loadChildren: () =>
+          import('../categorias/categorias.module').then(
+            (m) => m.CategoriasModule
+          ),
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class TemplateRoutingModule { }
+export class TemplateRoutingModule {}
