e0c9355bc44bf87798c3ede4522a555c779c5c78
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2025-05-01 18:27:27 -0300

    feat: implement reactive form for CategoriaComponent with validation

--- src/app/categorias/categoria/categoria.component.ts
+++ src/app/categorias/categoria/categoria.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-categoria',
@@ -8,12 +9,32 @@ import { Component } from '@angular/core';
 })
 export class CategoriaComponent {
 
+  camposFormulario: FormGroup;
 
+  constructor() {
+    this.camposFormulario = new FormGroup({
+      // Define your form controls here
+      nome : new FormControl('', Validators.required),
+      descricao : new FormControl('', Validators.required),
+    });
+  }
+
+  ngOnInit() {
+    // Initialize your form controls or fetch data here
+  }
 
   onSubmit() {
     // Handle form submission logic here
     console.log('Form submitted');
+    console.log(this.camposFormulario.value);
+    console.log('Esta valido:', this.camposFormulario.valid);
   }
+
+  isCampoValid(nomeCampo: string) : boolean {
+    const campo = this.camposFormulario.get(nomeCampo);
+    return (campo?.invalid && campo?.touched) || false;
+  }
+
   onReset() {
     // Handle form reset logic here
     console.log('Form reset');
@@ -21,6 +42,7 @@ export class CategoriaComponent {
   onCancel() {
     // Handle cancel action here
     console.log('Action cancelled');
+
   }
 
 }
